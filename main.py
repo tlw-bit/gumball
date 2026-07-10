dd23d2ad416fcddf174396166275e25b4662d74e
tlw-bit <ceb1301@gmail.com>
2026-07-10 19:20:38 +0100

    Update and rename main.py to index.json

--- main.py
+++
@@ -1,8 +0,0 @@
-color = {
-    "blue": 0x3498db,
-    "purple": 0x9b59b6,
-    "green": 0x2ecc71,
-    "lilac": 0xa884f7,
-    "golden": 0xFFD700
-}[group["id"]]
-await msg.edit(content=f"🎪 **Spinning...** {random.choice(['🔵','🟣','💚','🟪','✨'])}")
